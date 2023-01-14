5a02ad98c0a9086f7e928b55ea3d9e89f3c017e2
Schuyler deVos <schuylerdevos@gmail.com>
2023-01-14 15:42:02 -0500

    path function works

--- Unity/UnityProjects/SpatialMemories/Assets/Scripts/Custom/PathGenerator.cs
+++ Unity/UnityProjects/SpatialMemories/Assets/Scripts/Custom/PathGenerator.cs
@@ -11,29 +11,96 @@ public class PathGenerator : MonoBehaviour
     //search for coroutine 
     //vector from here to the 
     // Start is called before the first frame update
+
+    private enum PathGeneratorState
+    {
+        Unplayed,
+        PlayingSound,
+        ShowingNextPath,
+        Done
+    }
+    private PathGeneratorState pathGeneratorState = PathGeneratorState.Unplayed;
     public GameObject[] pathSpheres;
     public GameObject[] oldSpheres;
+
     public SphereCollider collider;
+
     public AudioSource audio;
+
     private bool didIStart = false;
     private bool memoriesAnnihilated = false;
+
+    private bool fadeOut = false;
+    private bool fadeIn = false;
+
+    public float fadeSpeed = 10.0f;
+
+    public float fadeInDuration = 3.0f;
+
+    private float currentFade = 0.0f;
+
     void Start()
     {
-        
+        this.gameObject.GetComponent<Renderer>().material.color = new Color(0f,0f,0f, 0.0f);
+        for (int i = 0; i < pathSpheres.Length; i++)
+        {
+            Color currentColorMaterial = pathSpheres[i].GetComponent<Renderer>().material.color;
+            Color objectColor = new Color(currentColorMaterial.r, currentColorMaterial.g, currentColorMaterial.b, 0.0f);
+            pathSpheres[i].GetComponent<Renderer>().material.color = objectColor;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (audio.isPlaying)
+        if (audio.isPlaying && pathGeneratorState == PathGeneratorState.Unplayed)
         {
-            //UnityEngine.Debug.Log("ok we are playing");
-            didIStart = true;
-            AnnihilateOldMemories();
-            //also disable trigger
+            UnityEngine.Debug.Log("move to playing sound");
+            pathGeneratorState = PathGeneratorState.PlayingSound;
         }
 
-        if (!audio.isPlaying && didIStart)
+        if (pathGeneratorState == PathGeneratorState.PlayingSound)
+        {
+            if (!audio.isPlaying)
+            {
+                UnityEngine.Debug.Log("move to showing path");
+                /*//UnityEngine.Debug.Log("ok we are playing");
+                didIStart = true;*/
+                //AnnihilateOldMemories();
+                //also disable trigger
+                pathGeneratorState = PathGeneratorState.ShowingNextPath;
+            }
+        }
+
+        if (pathGeneratorState == PathGeneratorState.ShowingNextPath)
+        {
+            //GeneratePaths();
+
+            //UnityEngine.Debug.Log("Showing path");
+
+            currentFade += Time.deltaTime;
+
+            float lerpedAlpha = Mathf.Lerp(0.0f, 1.0f, currentFade / fadeInDuration);
+
+            UnityEngine.Debug.Log("Lerped alpha is:" + lerpedAlpha);
+
+            for (int i = 0; i < pathSpheres.Length; i++)
+            {
+                Color currentColorMaterial = pathSpheres[i].GetComponent<Renderer>().material.color;
+                Color objectColor = new Color(currentColorMaterial.r, currentColorMaterial.g, currentColorMaterial.b, lerpedAlpha);
+                pathSpheres[i].GetComponent<Renderer>().material.color = objectColor;
+            }
+
+            if (lerpedAlpha >= 1.0f)
+            {
+                UnityEngine.Debug.Log("Done with path");
+                pathGeneratorState = PathGeneratorState.Done;
+            }
+
+        }
+
+
+        /*if (!audio.isPlaying && didIStart)
         {
             UnityEngine.Debug.Log("Ok we are done playing");
             didIStart = false;
@@ -42,7 +109,7 @@ public class PathGenerator : MonoBehaviour
             //and hide the current memory by getting a reference to self and turning off the mesh renderer
             MeshRenderer m = gameObject.GetComponent<MeshRenderer>();
             m.enabled = false;
-        }
+        }*/
 
     }
 
@@ -56,17 +123,10 @@ public class PathGenerator : MonoBehaviour
         }
     }
 
-    void AnnihilateOldMemories()
-    {
-        if (!memoriesAnnihilated)
-        {
-            for (int i = 0; i < oldSpheres.Length; i++)
-            {
-                //generate the spheres
-                MeshRenderer m = oldSpheres[i].GetComponent<MeshRenderer>();
-                m.enabled = false;
-            }
-            memoriesAnnihilated = true;
-        }
-    }
+        /*public void FadeOutObject() {
+        fadeOut = true;
+
+    public void FadeInObject() {
+        fadeIn = true;*/
+    
 }
